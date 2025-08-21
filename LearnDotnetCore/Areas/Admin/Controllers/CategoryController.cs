78551fc0754b9d32c07550925720f63aa74d382d
hieuhoa-dev <hieuhoa.dev.cf@gmail.com>
2025-08-21 07:47:23 +0700

    fix file name and Dockerfile
    Signed-off-by: hieuhoa-dev <hieuhoa.dev.cf@gmail.com>

---
+++ LearnDotnetCore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,104 @@
+﻿using learn_dotnet_core.DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+using learn_dotnet_core.Models;
+using learn_dotnet_core.DataAccess.Repository.IRepository;
+
+namespace learn_.net_core.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork db)
+        {
+            _unitOfWork = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> categoryList = _unitOfWork.Category.GetAll().ToList();
+
+            return View(categoryList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+            if (category.Name.ToLower() == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Tên danh mục không được trùng với thứ tự hiển thị");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(category);
+                _unitOfWork.Save();
+                TempData["Success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            //var categoryFromDb = _db.Categories.Find(id);
+            var categoryFromDb = _unitOfWork.Category.Get(u => u.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Category.Add(category);
+                _unitOfWork.Save();
+                TempData["Success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var categoryObj = _unitOfWork.Category.Get(u => u.Id == id);
+            if (categoryObj == null)
+            {
+                return NotFound();
+            }
+            return View(categoryObj);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeletePost(int id)
+        {
+            var categoryObj = _unitOfWork.Category.Get(u => u.Id == id);
+
+            if (categoryObj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Remove(categoryObj);
+            _unitOfWork.Save();
+            TempData["Success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LearnDotnetCore/Program.cs
@@ -0,0 +1,42 @@
+using DataAccess.Repository;
+using learn_dotnet_core.DataAccess.Data;
+using learn_dotnet_core.DataAccess.Repository;
+using learn_dotnet_core.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    option => option.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+// Register the repository service
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline. 
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
